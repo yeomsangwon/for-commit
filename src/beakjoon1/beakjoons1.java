745859a7a3720c2b918ec05b9da3121c37d6af09
6202-33 <6202-33@DESKTOP-JSO870E>
2019-09-16 16:50:28 +0900

    dddddd

--- src/beakjoon1/beakjoons1.java
+++ src/beakjoon1/beakjoons1.java
@@ -1,10 +1,35 @@
 package beakjoon1;
 
-public class beakjoons1 {
+import java.util.Scanner;
+
+public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int A,B,C, m,s;
+		
+		Scanner input=new Scanner(System.in);
+		
+		A=input.nextInt();
+		B=input.nextInt();
+		C=input.nextInt();
+		
+		m=A*B*C;
+		int []k=new int[10] ;
+		while(m!=0) {
+			s=m%10;
+			m=m/10;
+			k[s]+=1;
+			
+		}
+		for(int i=0; i<10;i++)
+		{
+			System.out.println(k[i]);
+		}
+		
+		
+	
+		
+	
 	}
-
 }
